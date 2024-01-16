d1871617bd3675addba6b71c7e9c93a4f9ac293f
Matheus-Moura26 <moura.matheus26@gmail.com>
2024-01-16 11:37:04 -0300

    Mais um exercicio do chatgpt resolvido

--- pergunta4Chatgpt.py
+++ pergunta4Chatgpt.py
@@ -2,4 +2,21 @@
 Crie um jogo simples onde os jogadores lançam dados. Registre os resultados
 dos jogadores em um dicionário, onde as chaves são os nomes dos jogadores e os
 valores são as somas dos valores dos dados."""
+import random
 
+v = "s"
+dicionario = dict()
+
+while v =="s":
+    nome = input("Qual o nome do jogador? ")
+    v = input("Deseja jogar dado? s/n: ")
+    if v == "s":
+        dado = random.randint(0,6)
+        print(f"Recebeu o valor {dado}")
+    else:
+        break
+    dicionario[nome] = dado
+    v = input("Há outro jogador para jogar? s/n: ")
+
+print(f"""Os seguintes jogadores obtiveram os seguintes valores:
+       {dicionario}""")
