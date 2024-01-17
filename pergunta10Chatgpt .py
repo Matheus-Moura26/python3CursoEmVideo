6db6def3e6c434e54005a01378aa18a0870d4336
Matheus-Moura26 <moura.matheus26@gmail.com>
2024-01-17 19:27:37 -0300

    gpt mandou exercicio repetido

---
+++ pergunta10Chatgpt .py
@@ -0,0 +1,6 @@
+"""Matriz Esparsa:
+Implemente uma matriz esparsa usando um dicionário onde as chaves são as
+coordenadas (linha, coluna) e os valores são os elementos da matriz. Realize
+operações de adição e multiplicação escalar."""
+
+#Este exercício já foi resolvido anteriormente
