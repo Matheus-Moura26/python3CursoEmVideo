26b37bba4525b9023864cb22eae5f74b200e1825
Matheus-Moura26 <moura.matheus26@gmail.com>
2024-01-17 17:55:49 -0300

    Rascunhos a respeito das informações dadas pelo chatgpt

--- rascunho.py
+++ rascunho.py
@@ -42,3 +42,53 @@ for k,v in dados.items():
     print(f"O {k} é {v}") """
 
 
+#NOVO RASCUNHO
+
+""" meuDicionario = {"Chave1": "Valor1","Chave2": "Valor2","Chave3": "Valor3","Chave4": "Valor4",}
+print(meuDicionario.get("Chave5" , "Não existe")) """
+
+#NOVO RASCUNHO
+""" 
+meuDicionario = {"Chave1": "Valor1","Chave2": "Valor2","Chave3": "Valor3","Chave4": "Valor4",}
+meuDicionario.setdefault("Chave5" , "KaKaKaKaKaKa")
+print(meuDicionario)
+ """
+
+#NOVO RASCUNHO
+
+""" meuDicionario = {"Chave1": "Valor1","Chave2": "Valor2","Chave3": "Valor3","Chave4": "Valor4",}
+meuDicionario.pop("Chave1", "ValorPadrão")
+print(meuDicionario) """
+
+
+#NOVO RASCUNHO
+""" 
+dicionario1 = {"a":1,"b":2,"c":3,}
+dicionario2 = {"d":4,"e":5,"f":6,}
+
+dicionario1.update(dicionario2)
+
+print(dicionario1)
+print(dicionario2)
+ """
+
+
+#NOVO RASCUNHO
+""" 
+dicionario1 = {"a":1,"b":2,"c":3,}
+dicionario2 = {"d":4,"e":5,"f":6,}
+
+dicionario1.update(dicionario2)
+
+print(dicionario1)
+print(dicionario2)
+"""
+
+
+#NOVO RASCUNHO
+
+""" 
+quadrado = {x: x**2 for x in range(1,100)}
+print(quadrado)
+ """
+
